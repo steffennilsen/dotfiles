cbf27ed8f444a8a360cd75ec27491e02ca26a733
steffen-nilsen-walwil <121027716+steffen-nilsen-walwil@users.noreply.github.com>
2022-12-21 14:05:16 +0000

    --wip-- [skip ci]

--- script/deps/ubuntu.sh
+++ script/deps/ubuntu.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-if [[ ! -x "$(command -v apt)" ]]; then
+if [ ! -x "$(command -v apt)" ]; then
     echo "apt not found" >&2
     exit 1
 fi
@@ -9,14 +9,14 @@ fi
 currdir="$(dirname -- ${BASH_SOURCE[0]})"
 files=$(grep -vE "^\s*#" "${currdir}/apt.packages.txt"  | tr "\n" " ")
 
-sudo bash<<EOF
+sudo /bin/sh<<EOF
 apt update
 apt install -y "$(files)"
 EOF
 
 # ubuntu often have an old version of git
 # get from official repo
-sudo bash<<EOF
+sudo /bin/sh<<EOF
 add-apt-repository -y ppa:git-core/ppa
 apt update
 apt install -y git
