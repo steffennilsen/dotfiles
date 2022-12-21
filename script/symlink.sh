c7c6f85e9549c0f9ac7db1f6aec69500eebbac73
steffen-nilsen-walwil <121027716+steffen-nilsen-walwil@users.noreply.github.com>
2022-12-21 14:00:25 +0000

    --wip-- [skip ci]

--- script/symlink.sh
+++ script/symlink.sh
@@ -3,15 +3,11 @@ set -e
 
 # $1 root path
 
-files=$(ls -a --ignore={".","..","script",".git",".gitignore","*.md"} "$1" \
-    | tr " " "\n" \
-    | sed -e "s/^/.\//" \
-    )
+files=$(ls -a $1 | grep -v -E "^[\.{1,2}|\.git|\.gitignore|readme.md]*$")
 
 # Create a symbolic link to each file in the home directory.
 for file in $files; do
     name="$(basename $file)"
-    echo "$name"
     
     [ -f "$HOME/$name" ] && rm -rf "$HOME/$name"
     ln -s "$1/$name" "$HOME/$name"
