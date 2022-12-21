12ff9231413ac4becdd390895ecc194896daf84e
steffen-nilsen-walwil <121027716+steffen-nilsen-walwil@users.noreply.github.com>
2022-12-21 14:13:26 +0000

    --wip-- [skip ci]

--- script/symlink.sh
+++ script/symlink.sh
@@ -3,7 +3,7 @@ set -e
 
 # $1 root path
 
-files=$(ls -a $1 | grep -v -E "^[\.{1,2}|\.git|\.gitignore|readme.md]*$")
+files=$(ls -a $1 | grep -v -E "^[\.{1,2}|\.git|\.gitignore|script|readme.md]*$")
 
 # Create a symbolic link to each file in the home directory.
 for file in $files; do
