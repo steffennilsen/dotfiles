f0a3dcdb30d39cb14590eb8af1980a0b2a47ff7c
steffen-nilsen-walwil <121027716+steffen-nilsen-walwil@users.noreply.github.com>
2022-12-22 10:41:56 +0000

    --wip-- [skip ci]

--- script/light.sh
+++ script/light.sh
@@ -4,17 +4,31 @@ set -e
 
 echo "installing dotfiles -- light"
 
-# make sure $HOME/bin and $HOME/.local/bin exists
-[ ! -d "$HOME/bin" ] && mkdir "$HOME/bin"
-[ ! -d "$HOME/.local" ] && mkdir "$HOME/.local"
-[ ! -d "$HOME/.local/bin" ] && mkdir "$HOME/.local/bin"
+light_watermark="$HOME/.config/dotfiles/installed-light"
+if [ -f "$light_watermark" ]; then
+  echo "dotfiles light install is already installed"
+  echo "remove $light_watermark to rerun this script"
+  exit 0
+fi
+
+# make sure certain directories exist
+makedir() {
+  [ ! -d "$1" ] && mkdir "$1" || true
+}
+
+makedir "$HOME/.config"
+makedir "$HOME/.config/dotfiles"
+makedir "$HOME/.local"
+makedir "$HOME/.local/bin"
+makedir "$HOME/bin"
 
 # symlink light files
 light="$1/light"
 files=$(ls -a "$light" -I '.' -I '..')
 echo "$files" | tr ' ' '\n' | while read file; do
-  [ -f "$HOME/$file" ] && rm -rf "$HOME/$file"
+  [ -f "$HOME/$file" ] && rm -r "$HOME/$file"
   ln -s "$light/$file" "$HOME/$file"
 done
 
+touch "$light_watermark"
 echo "dotfiles are installed"
