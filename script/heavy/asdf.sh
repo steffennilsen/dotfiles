eecbd7365d043b3e0ff2cb78ab6833a90a9f9e5c
steffen-nilsen-walwil <121027716+steffen-nilsen-walwil@users.noreply.github.com>
2022-12-22 15:05:18 +0000

    --wip-- [skip ci]

---
+++ script/heavy/asdf.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+# expand as needed, nodejs is my most used framework
+asdf_version='v0.11.0'
+default_nodejs_version='18.12.1'
+
+if [ ! -d "$HOME/.asdf" ]; then
+  echo 'installing asdf'
+  git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch "$asdf_version"
+fi
+
+PATH="$HOME/.asdf/bin:$PATH"
+
+echo "installing asdf plugins"
+[ -z 'asdf plugin list | grep nodejs' ] && asdf plugin add nodejs
+
+echo 'installing asdf frameworks'
+[ -z "asdf list nodejs | grep $default_nodejs_version" ] && asdf install nodejs "$default_nodejs_version"
+
+echo 'setting asdf globals'
+asdf global nodejs "$default_nodejs_version"
